b334db2cb5c0fbb53937e3e3baa93da76280a84b
thomas mouchelet <mouchelet.thomas@gmail.com>
2019-12-16 13:52:22 +0100

    init project

--- login.php
+++ login.php
@@ -1,1 +1,13 @@
 <?php require "head.php"; ?>
+
+<body>
+    <div class="form-container">
+        <h1>TINCAT</h1>
+        <form action="functions/getUser.php" method="post">
+            <input type="text" placeholder="pseudo" name="pseudo">
+            <input type="password" placeholder="password" name="password">
+            <input type="submit" value="login">
+        </form>
+    </div>
+</body>
+</html>
